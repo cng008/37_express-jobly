1b9727be978e07082a4514bcf1d85cc7805a8383
Christien Ng <ngchristien@gmail.com>
2022-05-24 19:00:54 -0700

    change double quotes to single quotes

--- middleware/auth.test.js
+++ middleware/auth.test.js
@@ -1,22 +1,17 @@
-"use strict";
+'use strict';
 
-const jwt = require("jsonwebtoken");
-const { UnauthorizedError } = require("../expressError");
-const {
-  authenticateJWT,
-  ensureLoggedIn,
-} = require("./auth");
+const jwt = require('jsonwebtoken');
+const { UnauthorizedError } = require('../expressError');
+const { authenticateJWT, ensureLoggedIn } = require('./auth');
 
+const { SECRET_KEY } = require('../config');
+const testJwt = jwt.sign({ username: 'test', isAdmin: false }, SECRET_KEY);
+const badJwt = jwt.sign({ username: 'test', isAdmin: false }, 'wrong');
 
-const { SECRET_KEY } = require("../config");
-const testJwt = jwt.sign({ username: "test", isAdmin: false }, SECRET_KEY);
-const badJwt = jwt.sign({ username: "test", isAdmin: false }, "wrong");
-
-
-describe("authenticateJWT", function () {
-  test("works: via header", function () {
+describe('authenticateJWT', function () {
+  test('works: via header', function () {
     expect.assertions(2);
-     //there are multiple ways to pass an authorization token, this is how you pass it in the header.
+    //there are multiple ways to pass an authorization token, this is how you pass it in the header.
     //this has been provided to show you another way to pass the token. you are only expected to read this code for this project.
     const req = { headers: { authorization: `Bearer ${testJwt}` } };
     const res = { locals: {} };
@@ -27,13 +22,13 @@ describe("authenticateJWT", function () {
     expect(res.locals).toEqual({
       user: {
         iat: expect.any(Number),
-        username: "test",
-        isAdmin: false,
-      },
+        username: 'test',
+        isAdmin: false
+      }
     });
   });
 
-  test("works: no header", function () {
+  test('works: no header', function () {
     expect.assertions(2);
     const req = {};
     const res = { locals: {} };
@@ -44,7 +39,7 @@ describe("authenticateJWT", function () {
     expect(res.locals).toEqual({});
   });
 
-  test("works: invalid token", function () {
+  test('works: invalid token', function () {
     expect.assertions(2);
     const req = { headers: { authorization: `Bearer ${badJwt}` } };
     const res = { locals: {} };
@@ -56,19 +51,18 @@ describe("authenticateJWT", function () {
   });
 });
 
-
-describe("ensureLoggedIn", function () {
-  test("works", function () {
+describe('ensureLoggedIn', function () {
+  test('works', function () {
     expect.assertions(1);
     const req = {};
-    const res = { locals: { user: { username: "test", is_admin: false } } };
+    const res = { locals: { user: { username: 'test', is_admin: false } } };
     const next = function (err) {
       expect(err).toBeFalsy();
     };
     ensureLoggedIn(req, res, next);
   });
 
-  test("unauth if no login", function () {
+  test('unauth if no login', function () {
     expect.assertions(1);
     const req = {};
     const res = { locals: {} };
